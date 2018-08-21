3bf4c7d66a1fd5e0c0ee543d57ff9fc59eda821d
rapsealk <piono623@naver.com>
2018-08-21 15:30:36 +0900

    화면 분할 옵션 추가

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/grid', (req, res) => {
+    const { size } = req.query;
+    console.log('size:', size);
+    res.render('grid', { size });
+});
+
+module.exports = router;
