5dd3f10142c8b59d43b43d78bfc7981bf1d2ff4d
rapsealk <piono623@naver.com>
2018-08-24 10:18:45 +0900

    ssh 접속 후 명령어 실행

--- sss.js
+++ sss.js
@@ -1,6 +1,6 @@
 const address = '192.168.0.53';
 console.log('address:', address);
-
+/*
 const sequest = require('sequest');
 
 const seq = sequest(`usvcam1@${address}`, {
@@ -10,6 +10,7 @@ const seq = sequest(`usvcam1@${address}`, {
     if (e) throw e;
     console.log(stdout.split('\n'));
 });
+*/
 
 /*
 const Client = require('ssh2').Client;
@@ -51,36 +52,37 @@ ssh.exec('ls', {
 }).start();
 */
 
-/*
-//const node_ssh = require('node-ssh');
-//const ssh = new node_ssh();
+const node_ssh = require('node-ssh');
+const ssh = new node_ssh();
 
 ssh.connect({
     host: address,
     port: 22,
-    username: 'usvcam1',
+    username: 'uavcam1',
     password: '111111',
 
+    /*
     tryKeyboard: true,
     onKeyboardInteractive: (name, instructions, instructionsLang, prompts, finish) => {
         if (prompts.length > 0 && prompts[0].prompt.toLowerCase().includes('password')) {
             finish([password]);
         }
     }
+    */
 })
 .then(() => {
     console.log('then ->');
-    //
+    /*
     ssh.exec('ifconfig', '', {
         onStdout: chunk => console.log(`stdout: ${chunk.toString('utf8')}`),
         onStderr: chunk => console.log(`stderr: ${chunk.toString('utf8')}`)
     });
-    //
+    */
+   ssh.exec('ifconfig')
+   .then(data => console.log('data:', data))
+   .catch(error => { throw error; });
 })
 .catch(error => {
     console.error(error);
     console.log('sakdjfglaksdjflaskdjflksd');
 });
-
-console.log('sdf');
-*/
