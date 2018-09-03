0442fdbab2be18b7880ae19b9fa133b9907b1117
DsLiner <rgdkdlel4#naver.com>
2018-09-04 03:44:59 +0900

    camera linked with pixel break

--- streampy/camera.py
+++ streampy/camera.py
@@ -6,28 +6,35 @@ import cv2
 from cv_bridge import CvBridge, CvBridgeError
 import roslibpy
 
+class message():
+    def __init__(self, message):
+        self.header = message['header']
+        self.height = message['height']
+        self.width = message['width']
+        self.encoding = "bgr8"
+        self.is_bigendian = message['is_bigendian']
+        self.step = message['step']
+        self.data = message['data']
+
 class VideoCamera(object):
     def __init__(self):
         pass
 
     def callback(self, data):
-        print('callback_called')
-        # try:
         bridge = CvBridge()
-        image = bridge.imgmsg_to_cv2(data, "bgr8")
-        # except CvBridgeError as e:
-        #     print(e)
-        print('image_changed')
+        try:
+            data_message = message(data)
+            image = bridge.imgmsg_to_cv2(data_message)
+        except CvBridgeError as e:
+            print(e)
         ret, jpeg = cv2.imencode('.jpg', image)
-        print(jpeg.tostring())
         self.conn.send(jpeg.tostring())
 
     def ros_run(self, conn):
         self.conn = conn
         ros = roslibpy.Ros(host='localhost', port=9090)
         ros.on_ready(lambda: print('Is ROS Connected?', ros.is_connected))
-        listener = roslibpy.Topic(ros, '/cv1/output_raw', 'sensor_msgs/Image')
-        listener.subscribe(self.callback)
+        roslibpy.Topic(ros, '/cv1/output_raw', 'sensor_msgs/Image').subscribe(self.callback)
         ros.run_forever()
 
 if __name__ == '__main__':
